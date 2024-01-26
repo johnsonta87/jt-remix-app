33f61656df3aeb0d5410154f7fefd78a5a5d1be6
Johnson <johnsonta87@gmail.com>
2024-01-26 12:46:28 -0500

    pages

--- app/components/ui/layout.tsx
+++ app/components/ui/layout.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export function Layout({ children }: Props) {
   return (
-    <div className="bg-white dark:bg-slate-950 text-slate-700 dark:text-white flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
+    <div className="bg-slate-100 dark:bg-slate-950 text-slate-700 dark:text-white flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
       {children}
     </div>
   );
