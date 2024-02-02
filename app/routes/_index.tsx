c20540da061b116ff92e91eb6013aba315ab75db
Johnson <johnsonta87@gmail.com>
2024-02-02 11:47:04 -0500

    cp

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,7 +12,7 @@ export default function Index() {
   return (
     <Hero
       title="Proactive, analytical and versatile Software Engineer with a passion for finding pragmatic solutions to technical challenges."
-      description="I build performant, scalable, accessible and modular applications with React, Vue and TypeScript. Currently in beautiful Canada."
+      description="I build performant, scalable, accessible and modular applications with React, Vue and TypeScript."
       cta={{
         text: "Learn More",
         link: "/about/",
